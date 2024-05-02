011dfed96ac931b1d853d8a95b110c6511a2eb25
Christ Mbuli <121105340+ChristMbuli@users.noreply.github.com>
2024-05-02 14:08:52 +0100

    Update config.php

--- backend/db/config.php
+++ backend/db/config.php
@@ -1,6 +1,6 @@
 <?php
 // Définition des constantes de connexion à la base de données
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'commerce');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'bx1qf87vcvpkiyuuwo1h-mysql.services.clever-cloud.com');
+define('DB_NAME', 'bx1qf87vcvpkiyuuwo1h');
+define('DB_USER', 'umswyyppnidm5hwo');
+define('DB_PASS', '4AlvBV8mbqUnP1GWMBLJ');

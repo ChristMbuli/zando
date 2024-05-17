3e220f91664e8f9ef3790b276ecdba3dff30ac35
Christ Mbuli <121105340+ChristMbuli@users.noreply.github.com>
2024-05-17 22:53:04 +0100

    Update config.php

--- backend/db/config.php
+++ backend/db/config.php
@@ -3,4 +3,4 @@
 define('DB_HOST', 'bx1qf87vcvpkiyuuwo1h-mysql.services.clever-cloud.com');
 define('DB_NAME', 'bx1qf87vcvpkiyuuwo1h');
 define('DB_USER', 'umswyyppnidm5hwo');
-define('DB_PASS', '4AlvBV8mbqUnP1GWMBLJ');
+define('DB_PASS', 'NAfqEUl9LEtyBh3I32Vi');
